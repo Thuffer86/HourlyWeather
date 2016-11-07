d024e77ddd7c61c4084586af8246eb11416cda97
Thuffer86 <Thuffer92@gmail.com>
2016-11-06 23:04:28 -0600

    fixes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
+function initGeolocation()
+     {
+        if( navigator.geolocation )
+        {
+           // Call getCurrentPosition with success and failure callbacks
+           navigator.geolocation.getCurrentPosition( success, fail );
+        }
+        else
+        {
+           alert("Sorry, your browser does not support geolocation services.");
+        }
+     }
 
-$('.temp').toggleClass(function() {
-    $(this).closest('.temp').find('.temp').toggleClass();
+     function success(position)
+     {
+
+        var positionLong  = position.coords.longitude;
+        var positionLat  = position.coords.latitude;
+		 		console.log(positionLat, positionLong);
+		 
+
+     }
+
+     function fail()
+     {
+		 console.log('Something went wrong');
+     }
+
+
+
+$('.temp').click(function() {
+	$('.temp').removeClass('active');
+    $(this).toggleClass('active');
     return false;
   });
+
+
