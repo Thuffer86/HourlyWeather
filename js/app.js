725d806c319f0554bcb30f1d0865fe94ec5be56a
Thuffer86 <Thuffer92@gmail.com>
2016-11-07 10:59:40 -0600

    fix

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(function() {
  //apply location data to webpage
 	$(".data__location").html(city);
 
-	  
+  });	  
 	  
 //GET DarkSky Data 
 	  $.getJSON("https://api.darksky.net/forecast/5cedbf3e31756f169d9dcd60b0549e09/" + latLong , function(weatherData) {
@@ -24,7 +24,7 @@ $(function() {
 		  $("#pos2Hr").html( pos1Temp + "&deg");
 
 	 });
-  });	
+	
 });
 
 
