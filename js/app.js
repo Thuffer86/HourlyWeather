a66ddaf7d3cd794c9f8f3ca389af48d1c4669e06
Thuffer86 <Thuffer92@gmail.com>
2016-11-07 10:31:34 -0600

    fix

--- js/app.js
+++ js/app.js
@@ -12,21 +12,17 @@ $(function() {
 	  
 //GET DarkSky Data 
 	  $.getJSON("https://api.darksky.net/forecast/5cedbf3e31756f169d9dcd60b0549e09/" + latLong , function(weatherData) {
-		  
 	var currentTemp = Math.round(weatherData.currently.apparentTemperature);
 	var neg1Temp = Math.round(weatherData.hourly.data[23].apparentTemperature);
 	var neg2Temp = Math.round(weatherData.hourly.data[22].apparentTemperature);
 	var pos1Temp = Math.round(weatherData.hourly.data[25].apparentTemperature);
 	var pos2Temp = Math.round(weatherData.hourly.data[26].apparentTemperature);
-
-		 console.log(weatherData);
 		  $("#currentHr").html( currentTemp + "&deg");
 		  $("#neg1Hr").html( neg1Temp + "&deg");
 		  $("#neg2Hr").html( neg2Temp + "&deg");
 		  $("#pos1Hr").html( pos1Temp + "&deg");
 		  $("#pos2Hr").html( pos1Temp + "&deg");
 
-		  
 	 });
   });	
 });
